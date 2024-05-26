68d000d58ee7680aea2cc7d6316946032267163f
Mubarak Arimiyah <abu_deedat@Mubaraks-MacBook-Pro.local>
2024-05-26 17:16:17 +0100

    Datakeeper: config and locator files are added in this change

--- src/main/java/baseContainer/TestBase.java
+++ src/main/java/baseContainer/TestBase.java
@@ -1,5 +1,9 @@
 package baseContainer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,14 +12,49 @@ import org.testng.annotations.BeforeTest;
 
 public class TestBase {
 	
-	public WebDriver driver; 
+	public WebDriver driver;
+	public Properties config = new Properties();  
+	public Properties locator = new Properties();
+	public FileInputStream file;
+	String ProjectLocation = System.getProperty("user.dir");
 	
 	@BeforeTest
 	public void Setup() {
+		
+		// Launching/Setups Browser
 		ChromeOptions options = new ChromeOptions();
 		driver = new ChromeDriver(options);
-		driver.get("https://demo.seleniumeasy.com/");
+		
+		//URL & Data file configuration Setup
+		try {
+			file = new FileInputStream(ProjectLocation+"/src/main/resources/propertyFiles/config.properties");
+		}catch(FileNotFoundException FNFE) {
+			FNFE.printStackTrace();
+		}
+		// Loading the file
+		try {
+			config.load(file);
+		}catch(IOException FNIO) {
+			FNIO.printStackTrace();
+		}
+		
+		// Locator File configuration
+		try {
+			file = new FileInputStream(ProjectLocation+"/src/main/resources/propertyFiles/locators.properties");
+		}catch(FileNotFoundException FNFE) {
+			FNFE.printStackTrace();
+		}
+		// Loading the file
+		try {
+			locator.load(file);
+		}catch(IOException FNIO) {
+			FNIO.printStackTrace();
+		}
+		
+		// Apllication Launching
+		driver.get(config.getProperty("url"));
 		driver.manage().window().maximize();
+		
 	}
 	
 	@AfterTest
--- src/main/java/tests/loginTest.java
+++ src/main/java/tests/loginTest.java
@@ -10,36 +10,36 @@ public class loginTest extends TestBase{
 	
 	@Test(priority = 2)
 	public void inputNumeric() {
-		driver.findElement(By.cssSelector("#user-message")).clear();
-		driver.findElement(By.cssSelector("#user-message")).sendKeys("2030");
-		driver.findElement(By.xpath("//button[contains(text(),'Show Message')]")).click();
+		driver.findElement(By.cssSelector(locator.getProperty("inputField"))).clear();
+		driver.findElement(By.cssSelector(locator.getProperty("inputField"))).sendKeys(config.getProperty("numeric"));
+		driver.findElement(By.xpath(locator.getProperty("showMessage"))).click();
 		String retriv = driver.findElement(By.id("display")).getText();
 		Assert.assertEquals(retriv, "2030");
 	}
 	
 	@Test (priority = 3)//(enabled = false)
-	public void inputAlphabet() {
-		driver.findElement(By.cssSelector("#user-message")).clear();
-		driver.findElement(By.cssSelector("#user-message")).sendKeys("Mubarak");
-		driver.findElement(By.xpath("//button[contains(text(),'Show Message')]")).click();
+	public void inputAlphabet() {		
+		driver.findElement(By.cssSelector(locator.getProperty("inputField"))).clear();
+		driver.findElement(By.cssSelector(locator.getProperty("inputField"))).sendKeys(config.getProperty("alphabet"));
+		driver.findElement(By.xpath(locator.getProperty("showMessage"))).click();
 		String retriv = driver.findElement(By.id("display")).getText();
 		Assert.assertEquals(retriv, "2030");
 	}
 	
 	@Test(priority = 1)
 	public void inputAlphaNumeric() {
-		driver.findElement(By.cssSelector("#user-message")).clear();
-		driver.findElement(By.cssSelector("#user-message")).sendKeys("Mubarak2030");
-		driver.findElement(By.xpath("//button[contains(text(),'Show Message')]")).click();
+		driver.findElement(By.cssSelector(locator.getProperty("inputField"))).clear();
+		driver.findElement(By.cssSelector(locator.getProperty("inputField"))).sendKeys(config.getProperty("alphaNumeric"));
+		driver.findElement(By.xpath(locator.getProperty("showMessage"))).click();
 	}
 	
 	@Test(priority = 0)
 	public void inputSpecialCharacter() {
-		driver.findElement(By.id("btn_basic_example")).click();
-		driver.findElement(By.linkText("Simple Form Demo")).click();
-		driver.findElement(By.cssSelector("#user-message")).clear();
-		driver.findElement(By.cssSelector("#user-message")).sendKeys("$$$$$$$$");
-		driver.findElement(By.xpath("//button[contains(text(),'Show Message')]")).click();
+		driver.findElement(By.id(locator.getProperty("startPractising"))).click();
+		driver.findElement(By.linkText(locator.getProperty("simpleFormDemo"))).click();
+		driver.findElement(By.cssSelector(locator.getProperty("inputField"))).clear();
+		driver.findElement(By.cssSelector(locator.getProperty("inputField"))).sendKeys(config.getProperty("specialCharacter"));
+		driver.findElement(By.xpath(locator.getProperty("showMessage"))).click();
 	}
 	
 	
